5313de7c8b90d70a2ae9ac61edfed990ccfa0738
bdoobd <G3xAg0N@mail.ru>
2026-08-19 15:24:39 +0000

    Temp commit

--- src/Core/App.php
+++ src/Core/App.php
@@ -4,16 +4,20 @@ namespace App\Core;
 
 class App
 {
+    public static App $app;
     public Router $router;
 
     public function __construct()
     {
+        self::$app = $this;
         $this->router = new Router();
     }
 
     public function run()
     {
         $uri = $_SERVER['REQUEST_URI'];
+        // $uri = rtrim($_SERVER['QUERY_STRING'], '\/');
+        // $uri = $_SERVER['QUERY_STRING'];
 
         $this->router->dispatch($uri);
     }
---
+++ src/Models/Case.php
@@ -0,0 +1,29 @@
+<?php  
+
+namespace App\Models;
+
+class Case {
+    public int $id = 0;
+    public int $case_id = 0;
+    public string $client = '';
+    public string $shipper = '';
+    public int $network_id = 0;
+    public int $method_id = 0;
+    public int $term_id = 0;
+    public int $destination_id = 0;
+    public datetime $start_date;
+    public datetime $departire_date;
+    public int $cargo_id = 0;
+    public int $transport_id = 0;
+    public int $document_id = 0;
+    public string $invoice_number = '';
+    public bool $archived = false;
+    public int $task_id = 0;
+    public string $comment = '';
+    public int $user_id = 0;
+    public int $status_id = 0;
+
+    public static function table_name(): string {
+        return 'cases';
+    }
+}
